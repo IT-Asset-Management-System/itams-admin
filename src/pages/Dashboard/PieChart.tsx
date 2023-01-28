f4490f8a897979405ee474781c7ef42ed2392be4
luongbs01 <54256832+luongbs01@users.noreply.github.com>
2023-01-28 21:58:37 +0700

    Feat: dashboard

--- src/pages/Dashboard/PieChart.tsx
+++ src/pages/Dashboard/PieChart.tsx
@@ -8,17 +8,6 @@ import {
 } from '@devexpress/dx-react-chart-material-ui';
 import { Animation } from '@devexpress/dx-react-chart';
 
-const data = [
-  { country: 'Russia', area: 12 },
-  { country: 'Canada', area: 7 },
-  { country: 'USA', area: 7 },
-  { country: 'China', area: 7 },
-  { country: 'Brazil', area: 6 },
-  { country: 'Australia', area: 5 },
-  { country: 'India', area: 2 },
-  { country: 'Others', area: 55 },
-];
-
 const Root = (props: any) => (
   <Legend.Root
     {...props}
@@ -37,12 +26,13 @@ const Label = (props: any) => (
   <Legend.Label {...props} sx={{ whiteSpace: 'wrap' }} />
 );
 
-export default function PieChart() {
+export default function PieChart(props: any) {
+  const { data } = props;
   return (
-    <Paper>
+    <Paper sx={{ minWidth: { md: '35vw' } }}>
       <Chart data={data}>
         <PieSeries valueField="area" argumentField="country" />
-        <Title text="Area of Countries" />
+        <Title text="Assets By Status" />
         <Legend
           position="bottom"
           rootComponent={Root}
